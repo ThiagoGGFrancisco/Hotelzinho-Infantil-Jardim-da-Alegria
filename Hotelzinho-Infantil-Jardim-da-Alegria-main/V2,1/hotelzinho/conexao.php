f1871acdd6da0cd8caf3770dc843cb15fecaac2a
wagner izaias vieira <94266707+wagnerizaias@users.noreply.github.com>
2023-05-01 09:55:47 -0300

    Add files via upload

--- Hotelzinho-Infantil-Jardim-da-Alegria-main/V2,1/hotelzinho/conexao.php
+++ Hotelzinho-Infantil-Jardim-da-Alegria-main/V2,1/hotelzinho/conexao.php
@@ -1,10 +1,10 @@
 
 <?php
 
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'aluno';
+$dbHost = '127.0.0.1:3306';
+$dbUsername = 'u849808817_escola';
+$dbPassword = 'Escola10';
+$dbName = 'u849808817_escola';
 
 $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
   if($conexao->connect_errno)
